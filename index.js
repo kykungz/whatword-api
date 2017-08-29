a030f75a339c2f12823d995c4954c2f5293b98c1
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2017-08-29 13:13:19 +0700

    remove build

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ require('babel-polyfill')
 const ip = require('ip')
 
 const PORT = process.env.PORT || 5000
-const env = process.env.NODE_ENV || 'development'
-const src = env === 'production' ? './build/server' : './src/server'
+// const env = process.env.NODE_ENV || 'development'
+// const src = env === 'production' ? './build/server' : './src/server'
+const src = './src/server'
 
 const server = require(src).default
 
